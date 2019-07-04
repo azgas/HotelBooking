885ff87aac981963bfb82176a31ac010c2298d92
Anna Zgaślik <azgaslik@gmail.com>
2019-07-04 20:42:20 +0200

    - added simple api project

---
+++ HotelApi/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HotelApi.Startup))]
+
+namespace HotelApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}",
+                defaults: new {id = RouteParameter.Optional});
+            app.UseWebApi(config);
+        }
+    }
+}
